c0f299fa3ada70bf1b1e396bdeb1cd9b649c42ff
Danylo Murzakoi <murzakojdaniilffff@gmail.com>
2025-06-13 18:02:47 +0300

    Final commit

---
+++ app/api/books/[id]/route.ts
@@ -0,0 +1,14 @@
+import books from "@/app/api/db";
+
+export async function PUT(
+  request: Request,
+  context: { params: { id: string }},
+
+) {
+  const id = context.params.id;
+  const book = await request.json();
+
+  const index = books.findIndex(b => b.id === id);
+  books[index] = book;
+  return Response.json(book);
+}
